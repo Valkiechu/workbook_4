88ecd138f729a5a622c668125b7a38325466ec67
Ethan Qiu <qethan1996@gmail.com>
2023-05-09 09:55:30 -0400

    finish the excer

---
+++ myfamily.js
@@ -0,0 +1,23 @@
+"use strick";
+let family = ["Ethan", "Mom", "Dad", "Grandma", "Grandpa"];
+
+// // // part 1
+// // let index = 0;
+
+// // //part 2
+// // while(index < family.length) {
+
+// //     console.log(family[index]);
+//     // let familyMemberName = family[index];
+//     // console.log(familyMemberName);
+
+//     //part 3
+
+//     index++;
+
+// }
+
+for (let index = 0; index < family.length; index++) {
+  let familyMemberName = family[index];
+  console.log(familyMemberName);
+}
